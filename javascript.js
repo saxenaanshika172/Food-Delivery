03ac7b4884135981282885309696dd55a4eed513
saxenaanshika172 <109079982+saxenaanshika172@users.noreply.github.com>
2023-11-13 21:54:13 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,42 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+ 
+
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+document.getElementById('login-btn').addEventListener('click', function() {
+    location.href = '/login page/HtmlCode.html';
+}, false);
+
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+document.getElementById('book-now').addEventListener('click', function() {
+    location.href ='/login page/HtmlCode.html';
+}, false);
